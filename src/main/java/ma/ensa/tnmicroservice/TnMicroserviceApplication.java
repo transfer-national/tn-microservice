fefbb402d9e967f4d5d50a171ee3fd9e876180ec
mohcine sahtani <mohcine.sahtani@gmail.com>
2023-12-02 16:55:56 +0100

    added client service

---
+++ src/main/java/ma/ensa/tnmicroservice/TnMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.tnmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ensa/tnmicroservice/TnMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.tnmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
