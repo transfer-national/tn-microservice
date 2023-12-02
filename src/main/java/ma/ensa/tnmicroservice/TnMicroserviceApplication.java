700426ff87432b1f74d39544711b0ab28f0f3967
mohcine sahtani <mohcine.sahtani@gmail.com>
2023-12-02 17:03:14 +0100

    initial commit

--- src/main/java/ma/ensa/tnmicroservice/TnMicroserviceApplication.java
+++ src/main/java/ma/ensa/tnmicroservice/TnMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TnMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TnMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TnMicroserviceApplication.class, args);
+    }
 
 }
