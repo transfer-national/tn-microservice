0f28a1b8280747f4e5e151fe3f80ed2ada5a95a7
mohcine sahtani <mohcine.sahtani@gmail.com>
2023-12-25 16:21:20 +0100

    creating email service

---
+++ email-service/src/main/java/ma/ensa/emailservice/AppConfig.java
@@ -0,0 +1,15 @@
+package ma.ensa.emailservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+
+        return new JavaMailSenderImpl();
+    }
+}
---
+++ email-service/src/main/java/ma/ensa/emailservice/EmailService.java
@@ -0,0 +1,16 @@
+package ma.ensa.emailservice;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+
+    private final JavaMailSender sender;
+
+}
